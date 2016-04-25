2e5e24f853578b0a955f53756cafc2ce488f1190
Szabó Gergő <gergo85@gmail.com>
2016-04-25 15:06:39 +0200

    Add Hungarian language

---
+++ lang/hu/lang.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Felhasználók menü',
+        'description' => 'A RainLab User bővítmény menüjének testreszabása.',
+    ],
+
+    'settings' => [
+        'label' => 'Oldalmenü',
+        'description' => 'Menüpontok testreszabása',
+    ],
+
+    'label' => 'Név',
+    'icon' => 'Ikon',
+    'order' => 'Sorrend',
+    'url' => 'Webcím',
+    'permissions' => 'Engedélyek',
+    'enabled' => 'Engedélyezve',
+
+    'base' => [
+        'tab' => 'Főmenüpont',
+        'enabled' => 'Menü felülírása',
+    ],
+
+    'first' => [
+        'tab' => 'Menüpont 1',
+        'enabled' => 'Menü engedélyezése',
+    ],
+
+    'second' => [
+        'tab' => 'Menüpont 2',
+        'enabled' => 'Menü engedélyezése',
+    ],
+
+    'third' => [
+        'tab' => 'Menüpont 3',
+        'enabled' => 'Menü engedélyezése',
+    ],
+
+    'fourth' => [
+        'tab' => 'Menüpont 4',
+        'enabled' => 'Menü engedélyezése',
+    ],
+
+    'fifth' => [
+        'tab' => 'Menüpont 5',
+        'enabled' => 'Menü engedélyezése',
+    ],
+
+    'sixt' => [
+        'tab' => 'Menüpont 6',
+        'enabled' => 'Menü engedélyezése',
+    ],
+];
